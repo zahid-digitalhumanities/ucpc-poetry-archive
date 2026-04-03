b08f64d35b59919aeff5252e9a8ee68c675b1362
zahid-digitalhumanities <zahidrana708@gmail.com>
2026-04-04 02:35:42 +0500

    Delete db.py

--- models/db.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-from config import DB_CONFIG
-
-def get_db_connection():
-    if 'dsn' in DB_CONFIG:
-        return psycopg2.connect(DB_CONFIG['dsn'])
-    return psycopg2.connect(**DB_CONFIG)
