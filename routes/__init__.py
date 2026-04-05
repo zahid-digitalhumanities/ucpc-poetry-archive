4f2fe5caed8133a9580f9349834fed38470c50f5
zahid-digitalhumanities <zahidrana708@gmail.com>
2026-04-05 17:51:20 +0500

    Delete routes directory

--- routes/__init__.py
+++
@@ -1,7 +0,0 @@
-﻿from .main_routes import main_bp
-from .poets_routes import poets_bp
-from .ghazals_routes import ghazals_bp
-# from .search_routes import search_bp   # uncomment when ready
-# from .bulk_routes import bulk_bp       # uncomment when ready
-
-all_blueprints = [main_bp, poets_bp, ghazals_bp]
