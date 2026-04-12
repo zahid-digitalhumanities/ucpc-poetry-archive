ceca19848cf9de197d41b0a484f7316a494999ad
zahid-digitalhumanities <zahidrana708@gmail.com>
2026-04-12 12:22:11 +0500

    update all

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,7 +1,6 @@
-﻿from .main_routes import main_bp
+from .main_routes import main_bp
 from .poets_routes import poets_bp
-#from .ghazals_routes import ghazals_bp
-# from .search_routes import search_bp   # uncomment when ready
-# from .bulk_routes import bulk_bp       # uncomment when ready
-
-all_blueprints = [main_bp, poets_bp]
+from .ghazals_routes import ghazals_bp
+from .search_routes import search_bp
+from .bulk_routes import bulk_bp
+from .listen_routes import listen_bp
