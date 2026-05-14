88ff4afe2a1a52b153d6ab722b69f893147d4804
zahid-digitalhumanities <zahidrana708@gmail.com>
2026-05-14 22:15:58 +0500

    Set environment variables for thread management

---
+++ startup.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
