680c5619fe8a96de7f3655398d5e091243bc86da
zahid-digitalhumanities <zahidrana708@gmail.com>
2026-05-15 19:01:36 +0500

    Refactor import statements in __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,12 +1,10 @@
 # routes/__init__.py
-# Lightweight version – only routes that actually exist
-
 from .main_routes import main_bp
 from .poets_routes import poets_bp
 from .ghazals_routes import ghazals_bp
 from .search_routes import search_bp
 
-# Only import these if the files exist
+# Optional – only if files exist
 try:
     from .ingest_routes import ingest_bp
 except ImportError:
@@ -26,5 +24,3 @@ try:
     from .listen_routes import listen_bp
 except ImportError:
     listen_bp = None
-
-# DO NOT import bulk_routes – file doesn't exist
