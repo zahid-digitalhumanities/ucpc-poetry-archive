687d0e09e82d33ce692f15b8c0980fdb5d5a84e7
zahid-digitalhumanities <zahidrana708@gmail.com>
2026-05-15 20:34:50 +0500

    Include poet_redirect_bp in routes
    Added poet_redirect_bp to the routes and updated __all__.

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,30 +1,7 @@
-# routes/__init__.py
-# Lightweight public archive – no AI imports
-
 from .main_routes import main_bp
 from .poets_routes import poets_bp
 from .ghazals_routes import ghazals_bp
 from .search_routes import search_bp
+from .poet_redirect import poet_redirect_bp
 
-# Optional routes (if files exist)
-try:
-    from .ingest_routes import ingest_bp
-except ImportError:
-    ingest_bp = None
-
-try:
-    from .corpus_routes import corpus_bp
-except ImportError:
-    corpus_bp = None
-
-try:
-    from .integrity_routes import integrity_bp
-except ImportError:
-    integrity_bp = None
-
-try:
-    from .listen_routes import listen_bp
-except ImportError:
-    listen_bp = None
-
-__all__ = ['main_bp', 'poets_bp', 'ghazals_bp', 'search_bp']
+__all__ = ['main_bp', 'poets_bp', 'ghazals_bp', 'search_bp', 'poet_redirect_bp']
