7fa582082fd18e6bd03bb4ae1e50f149803503b1
zahid-digitalhumanities <zahidrana708@gmail.com>
2026-05-16 19:25:06 +0500

    Delete semantic directory

--- semantic/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-UCPC Semantic Search Package
-Research-grade semantic retrieval for Urdu poetry
-"""
-
-from semantic.semantic_search_v2 import UCPCHybridSemanticEngine, search_semantic
-
-__all__ = ['UCPCHybridSemanticEngine', 'search_semantic']
