78930c16ad15a7eef4c383657cc65453b2c095ef
zahid-digitalhumanities <zahidrana708@gmail.com>
2026-05-16 19:24:34 +0500

    Delete routes directory

--- routes/__init__.py
+++
@@ -1,7 +0,0 @@
-from .main_routes import main_bp
-from .poets_routes import poets_bp
-from .ghazals_routes import ghazals_bp
-from .search_routes import search_bp
-from .poet_redirect import poet_redirect_bp
-
-__all__ = ['main_bp', 'poets_bp', 'ghazals_bp', 'search_bp', 'poet_redirect_bp']
