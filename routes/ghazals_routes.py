5ae30490e76935aa5480b575d9b7a0150a33e8cd
zahid-digitalhumanities <zahidrana708@gmail.com>
2026-05-20 13:22:32 +0500

    Update ghazals_routes.py

--- routes/ghazals_routes.py
+++ routes/ghazals_routes.py
@@ -1,50 +1,20 @@
-from flask import Blueprint
-from flask import render_template
-
+from flask import Blueprint, render_template
 from models.db import get_db
 
-ghazals_bp = Blueprint(
-    "ghazals",
-    __name__
-)
-
-# =====================================================
-# VIEW GHAZAL
-# =====================================================
+ghazals_bp = Blueprint("ghazals", __name__)
 
 @ghazals_bp.route("/view/<int:text_id>")
 def view_ghazal(text_id):
-
     conn = get_db()
-
     cur = conn.cursor()
 
-    cur.execute("""
-
-        SELECT *
-        FROM texts
-        WHERE id = %s
-
-    """, (text_id,))
-
+    cur.execute("SELECT * FROM texts WHERE id = %s", (text_id,))
     ghazal = cur.fetchone()
 
-    cur.execute("""
-
-        SELECT *
-        FROM verses
-        WHERE text_id = %s
-        ORDER BY verse_number ASC
-
-    """, (text_id,))
-
+    cur.execute("SELECT * FROM verses WHERE text_id = %s ORDER BY verse_number ASC", (text_id,))
     verses = cur.fetchall()
 
     cur.close()
     conn.close()
 
-    return render_template(
-        "view.html",
-        ghazal=ghazal,
-        verses=verses
-    )
+    return render_template("view.html", ghazal=ghazal, verses=verses)
