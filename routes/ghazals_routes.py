47b142260584307bd33e15f8c024855aab2b1d11
zahid-digitalhumanities <zahidrana708@gmail.com>
2026-05-20 14:32:02 +0500

    Fix string quotes in ghazals_routes.py

--- routes/ghazals_routes.py
+++ routes/ghazals_routes.py
@@ -1,10 +1,9 @@
 from flask import Blueprint, render_template, abort
 from models.db import get_db
 
-ghazals_bp = Blueprint("ghazals", __name__)
+ghazals_bp = Blueprint('ghazals', __name__)
 
-
-@ghazals_bp.route("/view/<int:text_id>")
+@ghazals_bp.route('/view/<int:text_id>')
 def view_ghazal(text_id):
     conn = get_db()
     cur = conn.cursor()
@@ -17,15 +16,10 @@ def view_ghazal(text_id):
         conn.close()
         abort(404)
 
-    cur.execute("""
-        SELECT * FROM verses 
-        WHERE text_id = %s 
-        ORDER BY verse_number ASC
-    """, (text_id,))
-    
+    cur.execute("SELECT * FROM verses WHERE text_id = %s ORDER BY verse_number ASC", (text_id,))
     verses = cur.fetchall()
 
     cur.close()
     conn.close()
 
-    return render_template("view.html", ghazal=ghazal, verses=verses)
+    return render_template('view.html', ghazal=ghazal, verses=verses)
