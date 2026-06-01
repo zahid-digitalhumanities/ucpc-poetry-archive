28528546c1b470373fa5cff8c9f8f2cd80b9ecbf
zahid-digitalhumanities <zahidrana708@gmail.com>
2026-06-01 20:26:24 +0500

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,1 +1,8 @@
-from routes.ghazals_routes import ghazals_bp
+# This file makes the routes directory a Python package
+# Import all blueprints to make them available
+from .main_routes import main_bp
+from .poets_routes import poets_bp
+from .ghazals_routes import ghazals_bp
+
+# List of all blueprints for easy access
+__all__ = ['main_bp', 'poets_bp', 'ghazals_bp']
