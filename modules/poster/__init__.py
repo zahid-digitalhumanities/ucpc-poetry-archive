1e16d60047bf4077589cccd11b694dc862f91668
zahid-digitalhumanities <zahidrana708@gmail.com>
2026-06-03 15:15:16 +0500

    Enhance module importability with additional exports

--- modules/poster/__init__.py
+++ modules/poster/__init__.py
@@ -1,1 +1,5 @@
-# This file makes the module importable
+# This file makes the module importable# This file makes the poster module importable
+from .poster_model import PosterModel
+from .poster_service import PosterService
+
+__all__ = ['PosterModel', 'PosterService']
