26acfb48cff470bc41cc9b3413d89116b745efb2
zahid-digitalhumanities <zahidrana708@gmail.com>
2026-06-03 15:18:43 +0500

    Delete __init__.py

--- modules/poster/__init__.py
+++
@@ -1,5 +0,0 @@
-# This file makes the module importable# This file makes the poster module importable
-from .poster_model import PosterModel
-from .poster_service import PosterService
-
-__all__ = ['PosterModel', 'PosterService']
